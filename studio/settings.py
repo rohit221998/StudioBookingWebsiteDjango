7f81092145667c19203a7597680ea65323c28c2b
rohit221998 <krohit221998@gmail.com>
2021-05-23 20:10:18 +0530

    DebugTrue

--- studio/settings.py
+++ studio/settings.py
@@ -24,7 +24,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','djangoappstudiobooking.azurewebsites.net','localhost']
 
