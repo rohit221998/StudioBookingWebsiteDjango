db69d45c0f232d18661f8866a4ee8e93a2c5cf3c
rohit221998 <krohit221998@gmail.com>
2021-05-30 12:09:11 +0530

    staticfile

--- studio/settings.py
+++ studio/settings.py
@@ -128,10 +128,12 @@ MEDIA_URL = '/media/'
 
 # added manually
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATIC_Root=os.path.join(BASE_DIR,"static")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
+
+
